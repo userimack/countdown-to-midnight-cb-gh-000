a48f678daf9b4fc8884ecf07b688fd48e5505944
Mahendra Yadav <mahendra.k12@gmail.com>
2018-04-30 11:32:01 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,4 +9,10 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-  
+  while  number > 0 do 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+    sleep(1)
+  end 
+  "HAPPY NEW YEAR!"
+end
